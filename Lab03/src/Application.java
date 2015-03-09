d90ced76ae972569188c10ca7fc650a2ea87d8d7
alusignan <am08390@ens.etsmtl.ca>
2015-03-09 17:02:03 -0400

    Test

--- Lab03/src/Application.java
+++ Lab03/src/Application.java
@@ -1,4 +1,22 @@
-
+/******************************************************
+Cours : LOG121
+Session : H2015
+Groupe : 01
+Projet : Laboratoire #3
+Étudiant(e)(s) : Jérôme L'Allier-Décary
+				 Alexandre Lusignan
+Code(s) perm. : LALJ26059304
+				LUSA11049100
+Professeur : Dominic St‐Jacques
+Chargés de labo : Alex Levesque et Simon Robert
+Nom du fichier : CollectionDes.java
+Date créé : 2015‐02-25
+Date dern. modif. 2015‐02-25
+*******************************************************
+Historique des modifications
+*******************************************************
+2015‐02‐25 Version initiale 
+*******************************************************/
 public class Application {
 
 	public static void main(String[] args) {
